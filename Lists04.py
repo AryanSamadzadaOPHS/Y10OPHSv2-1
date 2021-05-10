8894486a1f4af68c3f3382bd94a7df64bf0d08f4
LakshOPHS <68946802+LakshPatelOPHS1@users.noreply.github.com>
2021-05-10 11:41:21 +0100

    Create Lists04.py

---
+++ Lists04.py
@@ -0,0 +1,12 @@
+Tv = ["Attack on Titan", "Tokyo Ghoul", "Seven Deadly Sins", "One piece"]
+
+for i in Tv:
+    print(i)
+
+print(Tv)
+show = str(input("Enter another show "))
+
+Tv.append(show)
+
+for i in Tv:
+    print(i)
