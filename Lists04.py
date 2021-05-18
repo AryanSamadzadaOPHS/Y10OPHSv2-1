b2b07b8e517b738fe3db70f3635da23a34f7bf3f
LakshOPHS <68946802+LakshPatelOPHS1@users.noreply.github.com>
2021-05-18 11:32:21 +0100

    Update Lists04.py

--- Lists04.py
+++ Lists04.py
@@ -1,12 +1,14 @@
+
 Tv = ["Attack on Titan", "Tokyo Ghoul", "Seven Deadly Sins", "One piece"]
 
 for i in Tv:
     print(i)
 
-print(Tv)
 show = str(input("Enter another show "))
+pos = int(input("What position you want it? eg: 1,2,3... ")) - 1
 
-Tv.append(show)
+Tv.insert(pos, show)
 
 for i in Tv:
     print(i)
+
